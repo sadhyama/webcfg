55a242ab39861d923657c98c39858500f0315e1c
knair410 <Krishnan_Nair@comcast.com>
2020-04-22 17:11:25 +0530

    Identify BLOB, base64 encode and apply set

--- src/webcfg_multipart.c
+++ src/webcfg_multipart.c
@@ -465,15 +465,21 @@ WEBCFG_STATUS processMsgpackSubdoc(multipart_t *mp, char *transaction_id)
 			{
                                 if(pm->entries[i].value != NULL)
                                 {
-				if(0 == strncasecmp(mp->entries[m].name_space,"privatessid",strlen("privatessid")))
+				 if(pm->entries[i].type == WDMP_BLOB)
 				{
-					char *temp_blob = NULL;
+					char *temp_blob = NULL;					
 					
-					writebase64ToDBFile(WEBCFG_BASE64DB_FILE, pm->entries[i].value);			
-					temp_blob = base64blobdecoder(WEBCFG_BASE64DB_FILE);
+					temp_blob = base64blobencoder(pm->entries[i].value, pm->entries[i].value_size);
 					reqParam[i].name = strdup(pm->entries[i].name);
 				    	reqParam[i].value = strdup(temp_blob);
 				    	reqParam[i].type = pm->entries[i].type;
+					if(0 == strncasecmp(mp->entries[m].name_space,"portforwarding",strlen("portforwarding")))
+			{
+				WebcfgInfo("Updating X_RDK_PortMapping paramName\n");
+				reqParam[i].name = strdup("Device.NAT.X_RDK_PortMapping.Data");
+				WebcfgInfo("Updating DataType WDMP_BASE64\n");
+				reqParam[i].type = WDMP_BASE64;
+			}
 				}
 				else
 				{

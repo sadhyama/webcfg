42d2599c6930eda44db26505d98d577423ebca09
Sadhyama Vengilat <Sadhyama_Vengilat2@cable.comcast.com>
2020-08-06 16:33:23 +0530

    Move aker specific functions to aker file

--- src/webcfg_aker.h
+++ src/webcfg_aker.h
@@ -19,6 +19,7 @@
 #include "webcfg.h"
 #include <wrp-c.h>
 #include <libparodus.h>
+#include "webcfg_db.h"
 /*----------------------------------------------------------------------------*/
 /*                                   Macros                                   */
 /*----------------------------------------------------------------------------*/
@@ -39,4 +40,6 @@ WEBCFG_STATUS checkAkerStatus();
 void processAkerUpdateDelete(wrp_msg_t *wrpMsg);
 void processAkerRetrieve(wrp_msg_t *wrpMsg);
 libpd_instance_t get_webcfg_instance(void);
+AKER_STATUS processAkerSubdoc(webconfig_tmp_data_t *docNode, int akerIndex);
+void updateAkerMaxRetry(webconfig_tmp_data_t *temp, char *docname);
 #endif
